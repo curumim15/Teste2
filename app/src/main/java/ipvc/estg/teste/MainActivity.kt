c474eb43b44e9d54b2552c5cdcce2c98c94e9d8f
curumim15 <neca.dias28@gmail.com>
2022-02-22 15:12:32 +0000

    update (Teste Exemplo) 2

--- app/src/main/java/ipvc/estg/teste/MainActivity.kt
+++ app/src/main/java/ipvc/estg/teste/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)     // Teste exemplo2
+        super.onCreate(savedInstanceState)     // Teste exemplo exemplo
         setContentView(R.layout.activity_main) // Teste exemplo
     }
 }
